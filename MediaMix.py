c917c58fb39a89a7291faf12a30cd844486ef621
yw00k <yw00k64@gmail.com>
2025-08-12 20:57:21 +0900

    Add files via upload

--- MediaMix.py
+++ MediaMix.py
@@ -353,7 +353,7 @@ def optimize_total_budget(a_eok, b_eok, cprp_a, cpm_b, universe_val, unit=UNIT):
     ab_r1_curve = a_r1_curve * b_r1_curve
 
     X_opt = pd.DataFrame({'const': 0.0, 'r1_a': a_r1_curve, 'r1_b': b_r1_curve, 'r1_ab': ab_r1_curve})
-    total_r1_curve = model_total.predict(X_opt).values
+    total_r1_curve_raw = model_total.predict(X_opt).values
     total_r1_curve = plateau_after_exceed(total_r1_curve_raw, threshold=1.0)
 
     idx = int(np.argmax(total_r1_curve))
