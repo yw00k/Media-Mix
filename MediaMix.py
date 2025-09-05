4555a91680cceb38ff82d4756709174ac8780bd8
yw00k <yw00k64@gmail.com>
2025-09-06 01:43:47 +0900

    Add files via upload

--- MediaMix.py
+++ MediaMix.py
@@ -208,7 +208,7 @@ y_b3 = df_total.loc[dg_mask_r3, 'r3_b'].values
 def hill(x, a, b, c):
     return c / (1.0 + (b / x)**a)
 
-initial_params1 = [1.0, 25_000_000.0, 0.6]
+initial_params1 = [1.0, 25_000_000.0, 0.9]
 initial_params2 = [1.0, 25_000_000.0, 0.4]
 initial_params3 = [1.0, 25_000_000.0, 0.2]
 
@@ -218,7 +218,7 @@ bounds_b2 = ([0,0,0],[np.inf,np.inf,0.5])
 bounds_b3 = ([0,0,0],[np.inf,np.inf,0.3])
 
 popt_a1, _ = curve_fit(hill, x_a,  y_a1, p0=initial_params1, bounds=bounds_a, maxfev=20000)
-popt_b1, _ = curve_fit(hill, x_b,  y_b1, p0=initial_params1, bounds=bounds_b1, maxfev=20000)
+popt_b1, _ = curve_fit(hill, x_b,  y_b1, p0=initial_params1, bounds=bounds_a, maxfev=20000)
 popt_a2, _ = curve_fit(hill, x_a2, y_a2, p0=initial_params1, bounds=bounds_a, maxfev=20000)
 popt_b2, _ = curve_fit(hill, x_b2, y_b2, p0=initial_params2, bounds=bounds_b2, maxfev=20000)
 popt_a3, _ = curve_fit(hill, x_a3, y_a3, p0=initial_params1, bounds=bounds_a, maxfev=20000)
