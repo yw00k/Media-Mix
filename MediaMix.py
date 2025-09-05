29013291cc5ca9399abc8f4b9834f487247013aa
yw00k <yw00k64@gmail.com>
2025-09-05 22:58:46 +0900

    Add files via upload

--- MediaMix.py
+++ MediaMix.py
@@ -208,14 +208,14 @@ y_b3 = df_media.loc[dg_mask_r3, 'r3_b'].values
 def hill(x, a, b, c):
     return c / (1.0 + (b / x)**a)
 
-initial_params1 = [1.0, 25_000_000.0, 0.6]
+initial_params1 = [1.0, 25_000_000.0, 0.5]
 initial_params2 = [1.0, 25_000_000.0, 0.3]
-initial_params3 = [1.0, 25_000_000.0, 0.1]
+initial_params3 = [1.0, 25_000_000.0, 0.2]
 
 bounds_a = ([0,0,0],[np.inf,np.inf,1.0])
-bounds_b1 = ([0,0,0],[np.inf,np.inf,0.68])
-bounds_b2 = ([0,0,0],[np.inf,np.inf,0.4])
-bounds_b3 = ([0,0,0],[1.5 ,np.inf,0.3])
+bounds_b1 = ([0,0,0],[np.inf,np.inf,0.6])
+bounds_b2 = ([0,0,0],[np.inf,np.inf,0.45])
+bounds_b3 = ([0,0,0],[np.inf,np.inf,0.3])
 
 popt_a1, _ = curve_fit(hill, x_a,  y_a1, p0=initial_params1, bounds=bounds_a, maxfev=20000)
 popt_b1, _ = curve_fit(hill, x_b,  y_b1, p0=initial_params1, bounds=bounds_b1, maxfev=20000)
