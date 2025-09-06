2a417a4028bbfcc5cc8eed91930627e893029eae
yw00k <yw00k64@gmail.com>
2025-09-06 12:09:21 +0900

    Add files via upload

--- MediaMix.py
+++ MediaMix.py
@@ -465,10 +465,10 @@ def optimize_total_budget3(a_eok, b_eok, universe_val, unit=UNIT):
                       else b_r3_curve[idx3] if b3_share[idx3] >= 0.99
                       else total_r3_curve[idx3])
 
-    if total_r3_value < (a_r3_curve + b_r3_curve) :
-        total_r3_value = (a_r3_curve + b_r3_curve)
+    if total_r3_value[idx3] < (a_r3_curve[idx3] + b_r3_curve[idx3]) :
+        total_r3_value = (a_r3_curve[idx3] + b_r3_curve[idx3])
     else:
-        total_r3_value = total_r3_value
+        total_r3_value = total_r3_value[idx3]
 
     return {
         'a3_share': float(a3_share[idx3]), 'b3_share': float(b3_share[idx3]),
